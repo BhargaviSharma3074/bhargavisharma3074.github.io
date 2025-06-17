031cdab85b522972e2bd1c579e65fa2e17cbd79f
bhargavi.3074.lpu@gmail.com <bhargavi.3074.lpu@gmail.com>
2025-06-17 10:39:02 +0530

    login page edit

---
+++ Portfolio/P1/script.js
@@ -0,0 +1,35 @@
+const loginForm = () => {
+    const str = `
+    <h3>Login</h3>
+    <label>Email</label>
+    <input type='email' name='email' required><br><br>
+    <label>Password</label>
+    <input type='password' name='password' required><br><br>
+    <p><button onclick='showHome()'>Submit</button></p>
+    <p><button onclick='registerForm()'>Create Account</button></p>
+    `
+    root.innerHTML = str
+}
+
+const registerForm = () => {
+    const str = `<div>
+    <h3>Register</h3>
+    <label>Name</label>
+    <input type='text' name='name' required><br><br>
+    <label>Email</label>
+    <input type='email' name='email' required><br><br>
+    <label>Password</label>
+    <input type='password' name='password' required><br><br>
+    <p><button onclick='loginForm()'>Submit</button></p>
+    <p><button onclick='loginForm()'>Already a member? Login here</button></p>
+    `
+    root.innerHTML = str + "</div>"
+}
+
+const showHome = () => {
+    const str = `
+    <h3>Welcome!</h3>
+    <p><button>Logout</button></p>
+    `
+    root.innerHTML = str
+}
