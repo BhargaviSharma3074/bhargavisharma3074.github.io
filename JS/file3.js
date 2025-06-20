30ab5ced07c3d596d49cad897206e3b3db247030
bhargavi.3074.lpu@gmail.com <bhargavi.3074.lpu@gmail.com>
2025-06-20 10:24:05 +0530

    JS update

---
+++ JS/file3.js
@@ -0,0 +1,36 @@
+// let a = 10;
+// let b = 12;
+// // if(a>b)
+// // {
+// //     console.log(`${a} is greater than ${b}`);
+// // }
+// // else
+// // {
+// //     console.log(`${b} is greater than ${a}`);
+// // }
+// if(a>b) console.log(`${a} is greater than ${b}`);
+// else console.log(`${b} is greater than ${a}`);
+
+// let c;
+// if (c) console.log("Good Morning");
+// else console.log("Good Afternoon");
+
+// let c = null;
+// if (c) console.log("Good Morning");
+// else console.log("Good Afternoon");
+// null, undefined, false - will return in false
+
+// if (result) console.log("Good Morning");
+// else console.log("Good Afternoon");
+
+// let v1 = 10;
+// let v2 = 20;
+// let v3 = 15;
+// if(v1>v2 && v1>v3) console.log("v1 is greater");
+// else if(v2>v1 && v2>v3) console.log("v2 is greater");
+// else console.log("v3 is greater");
+
+let x = 5;
+let y = 2;
+// x>y ? console.log("x is greater") : console.log("y is greater");
+x > y && console.log("x is greater");
