18935f2c839605d1c849226e766f26374681fd3f
bhargavi.3074.lpu@gmail.com <bhargavi.3074.lpu@gmail.com>
2025-06-20 10:51:55 +0530

    JS update

--- JS/file5.js
+++ JS/file5.js
@@ -1,2 +1,26 @@
 // FUNCTIONS
 
+// function greet() 
+// {
+//     console.log("Good Morning");
+// }
+// greet();
+
+
+// IIFE method -> immediately invoke method
+// (function greet() 
+// {
+//     console.log("Good Morning");
+// })();
+
+// hoisting functionality -> js by default hoists the declaration to the top
+// greet();
+// function greet() 
+// {
+//     console.log("Good Morning");
+// }
+
+//hoisting in variables
+x = 10;
+console.log(x);
+var x;
---
+++ JS/file5a.js
@@ -0,0 +1,14 @@
+// FUNCTION ARGUMENTS AND RETURN 
+
+// function greet(student)
+// {
+//     console.log(`Hello ${student}!`);
+// }
+// greet("Bhargavi");
+
+function add(a,b)
+{
+    return a+b;
+}
+let sum = add(5,6);
+console.log(sum)
