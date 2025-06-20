dd5e8e793807d7cc186410e0716ef0926acf3549
bhargavi.3074.lpu@gmail.com <bhargavi.3074.lpu@gmail.com>
2025-06-20 11:54:37 +0530

    JS update

--- JS/file8.js
+++ JS/file8.js
@@ -10,6 +10,10 @@ const points = [2, 5, 4, 3, 7, 4, 8];
 //     console.log(value, index);
 // });
 
-points.forEach((value, index, arr) => {
-    console.log(value, index, arr);
+// points.forEach((value, index, arr) => {
+//     console.log(value, index, arr);
+// });
+
+points.forEach((a, b, c) => {
+    console.log(a);
 });
