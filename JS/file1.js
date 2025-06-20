d1ae7754f84c79c28bd011df943b430ced34c51c
bhargavi.3074.lpu@gmail.com <bhargavi.3074.lpu@gmail.com>
2025-06-20 09:41:41 +0530

    JS update

--- JS/file1.js
+++ JS/file1.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+let studentName = "John"
+let age = 20
+let promoted = true
+let score
+let city = null
+
+// console.log(studentName)
+// console.log(typeof studentName)
+// console.log(typeof score)           
+// console.log(typeof city)
+// console.log(typeof age)
+// let strAge = String(age) //typecasting
+// console.log(typeof strAge)
+// let rating = "5"
+// console.log(typeof rating)
+// let strRating = Number(rating)
+// console.log(typeof strRating)
+let flag = "true"
+let bFlag = Boolean(flag)
+console.log(typeof bFlag)
