b59a078e24460f34611f5b8e9f5209c48337002c
bhargavi.3074.lpu@gmail.com <bhargavi.3074.lpu@gmail.com>
2025-06-25 11:58:12 +0530

    MDB update

--- MONGODB/q4.js
+++ MONGODB/q4.js
@@ -22,9 +22,17 @@ db.employees.find(
     {name: 1}
 );
 
+db.employees.find({$and:[{salary: {$gt: 2000}, department: {$eq:"IT"}},{name:1}]})
+
 db.employees.find({salary:  {$gt: 1000}, department:{$eq: "IT"}}, {name: 1});
 
 db.employees.find({salary:  {$gt: 2000}, department:{$eq: "IT"}}, {name: 1}).limit(1);
 
+db.employees.find({$and:[{salary: {$gt: 3000}, department: {$eq: "IT"}}]});
+
+
 // display the top two highest paid employees
 db.employees.find().sort({salary:-1}).limit(2)
+
+// display name and email add of one name
+db.users.find({name:"Bhargavi"},{_id:0,email:1,name:1})
