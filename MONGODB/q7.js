e2155801352328caffabb3af4b8b471a14c934fe
bhargavi.3074.lpu@gmail.com <bhargavi.3074.lpu@gmail.com>
2025-06-26 11:29:20 +0530

    MDB update

--- MONGODB/q7.js
+++ MONGODB/q7.js
@@ -23,5 +23,14 @@ db.employees.aggregate([
 ]) 
 
 db.employees.aggregate([
-    {$project: {name:1, bonus:{$multiply:["$salary",2]}}},
+    {$project: {name:1, salary:1, bonus:{$multiply:["$salary",2]}}},
 ]) 
+
+// display name email and salary for all the it employees
+db.employees.aggregate({$match:{department:"IT"}},{$project:{_id:0,name:1, salary:1,email:1}})
+
+// display annual salary of all employees
+db.employees.aggregate({$project:{_id:0,name:1, salary:{$multiply:["$salary",12]}}})
+
+// display salary that is gt than 3000 and show ctc instead of salary
+db.employees.aggregate({$match:{salary: {$gt: 3000}}},{$project:{_id:0,ctc:"$salary"}})
